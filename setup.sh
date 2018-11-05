f77459d49893fa798089ef9edaa45c6ba7c8dc62
Ruairi O Brien <ruairitobrien@gmail.com>
2018-11-05 23:26:40 +0000

    Just overwrite the autostart file (being very lazy)

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get upgrade
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 sudo apt-get install --no-install-recommends chromium-browser
 
-sudo cat autostart >> /etc/xdg/openbox/autostart
+sudo cp autostart /etc/xdg/openbox/autostart
 
 echo "Put the following into .bash_profile and reboot"
 echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor"
